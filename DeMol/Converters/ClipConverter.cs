e8823fc6e35d3541a2d3cd4f138b3bbedea21527
Jan Verley <janverley@gmail.com>
2025-06-10 00:02:21 +0200

    ronde fotos

---
+++ DeMol/Converters/ClipConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DeMol.Converters
+{
+    public class ClipConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values?.Length == 2 && values[0] is double width && values[1] is double height)
+            {
+                var ellipseGeometry = new EllipseGeometry
+                {
+                    Center = new Point(width / 2, height / 2),
+                    RadiusX = width / 2,
+                    RadiusY = height / 2
+                };
+                return ellipseGeometry;
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
