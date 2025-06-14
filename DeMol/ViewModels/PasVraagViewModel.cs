76997b886c4c40424d16b91b3eccbe4753a7c5f6
Jan Verley <Jan.Verley@agidens.com>
2025-06-14 21:56:16 +0200

    invullen molletje juist per dag

--- DeMol/ViewModels/PasVraagViewModel.cs
+++ DeMol/ViewModels/PasVraagViewModel.cs
@@ -18,5 +18,16 @@ namespace DeMol.ViewModels
             get => pasvragenVerdiend;
             set => Set(ref pasvragenVerdiend, value);
         }
+        public bool Dag1MolOk { get; set; }
+        public bool Dag1MolletjeOk { get; set; }
+        public bool Dag2MolOk { get; set; }
+        public bool Dag2MolletjeOk { get; set; }
+        public bool Dag3MolOk { get; set; }
+        public bool Dag3MolletjeOk { get; set; }
+        public bool Dag4MolOk { get; set; }
+        public bool Dag4MolletjeOk { get; set; }
+        public bool Dag5MolOk { get; set; }
+        public bool Dag5MolletjeOk { get; set; }
+
     }
 }
